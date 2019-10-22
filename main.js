b56fe628dcc68989ebb4d3fb8675e62cf4121584
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-22 14:29:01 -0400

    first one done

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function doubleAll() {
+function doubleAll(arr) {
+  const newArr = []
+  for (i=0;i<arr.length;i++) {
+    newArr.push(arr[i] * 2)
+  }
+  return newArr
 }
 
 function absoluteValues() {
